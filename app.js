341f8278d656142c6c27432e072da6c23b25a5ba
AndrewTillott <andytillott@hotmail.co.uk>
2020-07-22 12:22:57 +0100

    Sum All Primes

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+/*
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+*/
+
+function sumPrimes(num) {
+  let arr = [];
+  for(let i = 2; i <= num; i++){
+      isPrime(i);
+  }
+
+  function isPrime(num) {
+  var sqrtnum=Math.floor(Math.sqrt(num));
+    var prime = num != 1;
+    for(var i=2; i<sqrtnum+1; i++) { // sqrtnum+1
+        if(num % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+      if(prime){arr.push(num)};
+}
+
+  return arr.reduce((total, item)=>{
+    return total + item;
+  }, 0);
+}
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
+
 
 /*
 Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
