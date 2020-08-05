92e432ad4c2dfcfa9c51715e30ddf45f2de43e23
AndrewTillott <andytillott@hotmail.co.uk>
2020-08-05 11:46:26 +0100

    Area of an arrow commit

--- app.js
+++ app.js
@@ -236,3 +236,17 @@ function pairElement(str) {
       console.log(fearNotLetter("abce"));
       console.log(fearNotLetter("stvwx"));
       console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"))
+
+
+      /*An arrow is formed in 
+      a rectangle with sides a and b by joining the bottom corners to 
+      the midpoint of the top edge and the centre of the rectangle.
+
+      a and b are integers and > 0
+
+Write a function which returns the area of the arrow.
+*/
+
+function arrowArea(a,b) {
+  return ((a * (b/2)) / 2)
+}
